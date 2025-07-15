952cdf0d7d565595bda0f8889904fbb0a49e18ae
ritapatacas <ritsaptcs@gmail.com>
2025-07-15 15:36:38 +0100

    try making 404 page a "standalone" page 2.0

---
+++ app/404/layout.tsx
@@ -0,0 +1,9 @@
+export default function NotFoundLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="pt">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+} 
