5fd0fb1246100509eec83b5cf0e535bbfd9c7899
ritapatacas <ritsaptcs@gmail.com>
2025-07-15 15:23:23 +0100

    update 404 (useSearchParams error)

---
+++ app/404.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div style={{ padding: 40, textAlign: "center" }}>
+      <h1>Página não encontrada</h1>
+      <p>Desculpe, não conseguimos encontrar o que procura.</p>
+    </div>
+  );
+} 
